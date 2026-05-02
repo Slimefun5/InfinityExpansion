4227b004e0cdaba7780c528381aac6bad38a5784
Intisy <finn@birich.de>
2026-05-02 23:54:30 +0200

    Remove temporary python scripts

--- git_push.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-os.chdir(r'D:\Documents\GitHub\slimefun\InfinityExpansion')
-subprocess.run(['git', 'status'])
-subprocess.run(['git', 'add', '.'])
-subprocess.run(['git', 'commit', '-m', 'Fix Slimefun5 API compatibility errors'])
-subprocess.run(['git', 'push', 'origin', 'master'])
-print("Pushed")
