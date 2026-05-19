eca467b6b1da9d4e181113bdc73ba19b3fcae335
Intisy <finn@birich.de>
2026-05-19 17:02:48 +0200

    fix: switch to JitPack for SlimefunMetrics dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
 }
 
 dependencies {
-    githubImplementation("Slimefun5:SlimefunMetrics:master")
+    implementation("com.github.Slimefun5:SlimefunMetrics:master-SNAPSHOT")
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     "githubCompileOnly"("Slimefun5:Slimefun5:v5.0.3")
