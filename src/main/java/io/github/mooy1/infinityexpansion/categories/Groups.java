70faff09b5024335b99f6c7fcfef77494bafd4c8
intisy (Finn Birich) <finn@birich.de>
2026-07-22 13:34:14 +0200

    feat(guide): declare categories, drop custom guide UIs, surface item groups

--- src/main/java/io/github/mooy1/infinityexpansion/categories/Groups.java
+++ src/main/java/io/github/mooy1/infinityexpansion/categories/Groups.java
@@ -1,11 +1,8 @@
 package io.github.mooy1.infinityexpansion.categories;
 
-import org.bukkit.Material;
-
 import io.github.mooy1.infinityexpansion.InfinityExpansion;
 import io.github.mooy1.infinityexpansion.MaterialCompat;
 import io.github.thebusybiscuit.slimefun5.libraries.xseries.XMaterial;
-import io.github.mooy1.infinitylib.groups.MultiGroup;
 import io.github.mooy1.infinitylib.groups.SubGroup;
 import io.github.thebusybiscuit.slimefun5.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun5.libraries.dough.items.CustomItemStack;
@@ -17,29 +14,32 @@ import io.github.thebusybiscuit.slimefun5.libraries.dough.items.CustomItemStack;
  */
 public final class Groups {
 
+    // Kept for InfinityGroup's own references; no longer registered (custom guide UIs are deprecated).
     public static final ItemGroup INFINITY = new InfinityGroup(InfinityExpansion.createKey("infinity_recipes"),
             CustomItemStack.create(MaterialCompat.safe(XMaterial.RESPAWN_ANCHOR), "&bInfinity &7Recipes"), 3).setTheme("machines");
-    public static final ItemGroup MAIN_MATERIALS = new SubGroup("main_materials",
+    public static final ItemGroup MAIN_MATERIALS = new ItemGroup(InfinityExpansion.createKey("main_materials"),
             CustomItemStack.create(MaterialCompat.safe(XMaterial.NETHER_STAR), "&bInfinity &7Materials"));
-    public static final ItemGroup BASIC_MACHINES = new SubGroup("basic_machines",
+    public static final ItemGroup BASIC_MACHINES = new ItemGroup(InfinityExpansion.createKey("basic_machines"),
             CustomItemStack.create(MaterialCompat.safe(XMaterial.LOOM), "&9Basic &7Machines"));
-    public static final ItemGroup ADVANCED_MACHINES = new SubGroup("advanced_machines",
+    public static final ItemGroup ADVANCED_MACHINES = new ItemGroup(InfinityExpansion.createKey("advanced_machines"),
             CustomItemStack.create(MaterialCompat.safe(XMaterial.BLAST_FURNACE), "&cAdvanced &7Machines"));
-    public static final ItemGroup STORAGE = new SubGroup("storage",
+    public static final ItemGroup STORAGE = new ItemGroup(InfinityExpansion.createKey("storage"),
             CustomItemStack.create(MaterialCompat.safe(XMaterial.BEEHIVE), "&6Storage"));
-    public static final ItemGroup MOB_SIMULATION = new SubGroup("mob_simulation",
+    public static final ItemGroup MOB_SIMULATION = new ItemGroup(InfinityExpansion.createKey("mob_simulation"),
             CustomItemStack.create(MaterialCompat.safe(XMaterial.BEACON), "&bMob Simulation"));
-    public static final ItemGroup INFINITY_MATERIALS = new SubGroup("infinity_materials",
+    public static final ItemGroup INFINITY_MATERIALS = new ItemGroup(InfinityExpansion.createKey("infinity_materials"),
             CustomItemStack.create(MaterialCompat.safe(XMaterial.NETHERITE_BLOCK), "&bInfinity &aMaterials"));
-    public static final ItemGroup MAIN_CATEGORY = new MultiGroup("main",
-            CustomItemStack.create(MaterialCompat.safe(XMaterial.NETHER_STAR), "&bInfinity &7Expansion"), 3,
-            MAIN_MATERIALS, BASIC_MACHINES, ADVANCED_MACHINES, STORAGE, MOB_SIMULATION, INFINITY_MATERIALS, INFINITY).setTheme("machines");
+    // Hidden: holds intentionally-incorrect infinity recipes.
     public static final ItemGroup INFINITY_CHEAT = new SubGroup("infinity_cheat",
             CustomItemStack.create(MaterialCompat.safe(XMaterial.RESPAWN_ANCHOR), "&bInfinity &7Recipes &c- INCORRECT RECIPES")).setTheme("machines");
 
     public static void setup(InfinityExpansion inst) {
-        INFINITY.register(inst);
-        MAIN_CATEGORY.register(inst);
+        MAIN_MATERIALS.register(inst);
+        BASIC_MACHINES.register(inst);
+        ADVANCED_MACHINES.register(inst);
+        STORAGE.register(inst);
+        MOB_SIMULATION.register(inst);
+        INFINITY_MATERIALS.register(inst);
         MOB_SIMULATION.setCrossAddonItemGroup(true);
         INFINITY_CHEAT.register(inst);
     }
